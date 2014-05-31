7b33edad74313dfe52c25134400bd96983e5a935
Raphael Javaux <raphaeljavaux@gmail.com>
2014-05-31 18:01:50 +0200

    edge detection

--- src/Vision/Detector/Edge.hs
+++ src/Vision/Detector/Edge.hs
@@ -8,7 +8,9 @@ import Vision.Image.Interpolate (bilinearInterpol)
 -- See <http://en.wikipedia.org/wiki/Canny_edge_detector> for details.
 -- canny :: (Image i, ImagePixel i ~ GreyPixel) => i
 canny !img !radius !mSig !lowThres !highThres=
-    let blurred        :: GreyImage
+    let !size@(Z :. h :. w) = shape img
+
+        blurred        :: GreyImage
         gaussianFilter :: SeparableFilter src Float res
         !gaussianFilter = gaussianBlur radius mSig
         !blurred        = img `I.apply` gaussianFilter
@@ -19,24 +21,29 @@ canny !img !radius !mSig !lowThres !highThres=
 
         -- Magnitude of the gradient, squared.
         dxy :: Manifest Int32
-        !dxy = fromFunction (shape img) $ \!pt ->
+        !dxy = fromFunction size $ \!pt ->
                     square (dx `index` pt) + square (dy `index` pt)
 
         -- Direction of the gradient.
         angle :: Manifest Float
-        !angle = fromFunction (shape img) $ \!pt ->
+        !angle = fromFunction size $ \!pt ->
                     atan2 (dx `index` pt) (dy `index` pt)
-    in
 
+    edges = new' (h * w) 0
+    forM_ (enumFromN 0 h) $ \y -> do
+        forM_ (enumFromN 0 w) $ \x -> do
+            
   where
-    isMaxima !x !y magn !dx !dy =
-        let !dx'  = dx % magn
-            !dy'  = dy % magn
-            !x'   = ratio x
-            !y'   = ratio y
-            !left = RPoint (x' + dx') (y' + dy')
+    isMaxima !x !y !dxy !dx !dy =
+        let dx'  = dx % magn
+            dy'  = dy % magn
+            x'   = ratio x
+            y'   = ratio y
+            left  = RPoint (x' + dx') (y' + dy')
             right = RPoint (x' - dx') (y' - dy')
-        in    dxy `bilinearInterpol` left  < magn
+        in if | left >= 0
+            
+            dxy `bilinearInterpol` left  < magn
            && dxy `bilinearInterpol` right < magn
 
 square :: Num a => a -> a
